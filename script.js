6a6719421c402616c36620e9a3b140dee61fecf0
iamsxntx <daleonpa@unal.edu.co>
2025-02-15 19:54:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const requisitosCultivos = {
     mora: { luminosidad: "6-8 horas", humedad: "60-70%", temperatura: "15-25°C" },
-    lulo: { luminosidad: "8-10 horas", precipitacion: "1000-1500 mm", humedad: "70-80%", temperatura: "15-20°C" },
-    frijol: { luminosidad: "6-8 horas", precipitacion: "500-800 mm", humedad: "50-60%", temperatura: "20-30°C" },
-    cafe: { luminosidad: "5-7 horas", precipitacion: "1000-1500 mm", humedad: "70-80%", temperatura: "18-24°C" },
-    maiz: { luminosidad: "10-12 horas", precipitacion: "600-800 mm", humedad: "55-75%", temperatura: "20-30°C" },
-    arveja: { luminosidad: "6-8 horas", precipitacion: "500-600 mm", humedad: "50-70%", temperatura: "15-20°C" },
-    yuca: { luminosidad: "8-10 horas", precipitacion: "1000-1200 mm", humedad: "60-70%", temperatura: "25-30°C" },
-    auyama: { luminosidad: "6-8 horas", precipitacion: "800-1000 mm", humedad: "60-70%", temperatura: "20-25°C" },
-    papa: { luminosidad: "8-10 horas", precipitacion: "600-800 mm", humedad: "70-80%", temperatura: "15-20°C" },
-    cebolla: { luminosidad: "10-12 horas", precipitacion: "500-600 mm", humedad: "60-70%", temperatura: "15-20°C" },
-    tomate: { luminosidad: "8-10 horas", precipitacion: "600-800 mm", humedad: "60-70%", temperatura: "20-25°C" },
-    naranjas: { luminosidad: "8-10 horas", precipitacion: "600-800 mm", humedad: "50-60%", temperatura: "25-30°C" },
+    lulo: { luminosidad: "8-10 horas", humedad: "70-80%", temperatura: "15-20°C" },
+    frijol: { luminosidad: "6-8 horas", humedad: "50-60%", temperatura: "20-30°C" },
+    cafe: { luminosidad: "5-7 horas",  humedad: "70-80%", temperatura: "18-24°C" },
+    maiz: { luminosidad: "10-12 horas", humedad: "55-75%", temperatura: "20-30°C" },
+    arveja: { luminosidad: "6-8 horas", humedad: "50-70%", temperatura: "15-20°C" },
+    yuca: { luminosidad: "8-10 horas", humedad: "60-70%", temperatura: "25-30°C" },
+    auyama: { luminosidad: "6-8 horas", humedad: "60-70%", temperatura: "20-25°C" },
+    papa: { luminosidad: "8-10 horas", humedad: "70-80%", temperatura: "15-20°C" },
+    cebolla: { luminosidad: "10-12 horas", humedad: "60-70%", temperatura: "15-20°C" },
+    tomate: { luminosidad: "8-10 horas", humedad: "60-70%", temperatura: "20-25°C" },
+    naranjas: { luminosidad: "8-10 horas", humedad: "50-60%", temperatura: "25-30°C" },
 };
 
 let chart;
@@ -25,7 +25,6 @@ function analizarCultivo() {
         <h3>Requisitos para cultivar ${cultivo.charAt(0).toUpperCase() + cultivo.slice(1)}:</h3>
         <ul>
             <li><strong>Luminosidad:</strong> ${requisitos.luminosidad}</li>
-            <li><strong>Precipitación:</strong> ${requisitos.precipitacion}</li>
             <li><strong>Humedad:</strong> ${requisitos.humedad}</li>
             <li><strong>Temperatura:</strong> ${requisitos.temperatura}</li>
         </ul>
@@ -64,7 +63,6 @@ function mostrarGrafico(requisitos) {
                     label: 'Requisitos óptimos',
                     data: [
                         parseFloat(requisitos.luminosidad.split('-')[0]),
-                        parseFloat(requisitos.precipitacion.split('-')[0]),
                         parseFloat(requisitos.humedad.split('-')[0]),
                         parseFloat(requisitos.temperatura.split('-')[0])
                     ],
